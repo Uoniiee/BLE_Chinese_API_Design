49130888194c1eb479bc9db9002e8c18f031d3ce
Uoniiee <283946599+Uoniiee@users.noreply.github.com>
2026-05-29 18:18:11 +0800

    Keep sample app stable on rotation

--- sample_app/src/main/java/com/uoniiee/blechineseapi/sample/MainActivity.kt
+++ sample_app/src/main/java/com/uoniiee/blechineseapi/sample/MainActivity.kt
@@ -35,7 +35,7 @@ import java.util.Locale
 class MainActivity : Activity() {
 
     private companion object {
-        const val 示例版本 = "v0.5.8-debug"
+        const val 示例版本 = "v0.5.9-debug"
     }
 
     private val 作用域 = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
